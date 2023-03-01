8a33401b72a48d57da7420d472c18bcc8f517008
Angelo Neacsu <angelo.neacsu001@xpo.com>
2023-03-01 18:29:10 +0200

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
+JEKYLL_ENV=production bundle exec jekyll build
 tar zcf _site.tgz _site/;
 scp _site.tgz root@vm1.sysbox.pro:/opt/docs/
 ssh -l root vm1.sysbox.pro 'tar xf /opt/docs/_site.tgz -C /opt/docs/'
 ssh -l root vm1.sysbox.pro 'docker-compose -f /opt/docs/docs.yaml up -d'
+rm -f _site.tgz
